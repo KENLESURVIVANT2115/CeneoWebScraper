f487487a10ea880f6b7393c6671e1ad380fbf05c
KENLESURVIVANT2115 <Hubertgurgul@gmail.com>
2024-05-08 19:39:11 +0200

    2024-05-08 H G

---
+++ app/routes.py
@@ -0,0 +1,13 @@
+from app import app
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return "Hello, World!"
+
+@app.route("/hello")
+@app.route('/hello/<name>')
+def hello(name='Anonymous'):
+    return f"Hello, {name}!"
+
+
